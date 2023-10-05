d43ed2c1823295904d7671daf68125168b12bbb3
SukhTheCreator19 <143115777+SukhTheCreator19@users.noreply.github.com>
2023-10-05 17:54:23 +0000

    REahh

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 #Sukhmaan Singh
-text = str(input("Import any piece of text: ")).lower() #Asks user for text
-first = str(input("Enter First Letter: ")).lower() #Asks the user for a letter to search in text
-print("First Letter:", first) #Prints the letter provided
-second = str(input("Enter Second Letter: ")).lower() #Asks the user for a letter to search in text
-print("First Letter:", second) #Prints the letter provided
-third = str(input("Enter Third Letter: ")).lower()#Asks the user for a letter to search in text
-print("Third Letter:", third) #Prints the letter provided
+text = str(input("Import any piece of text: ")).lower()  #Asks user for text
+first = str(input("Enter First Letter: ")).lower(
+)  #Asks the user for a letter to search in text
+print("First Letter:", first)  #Prints the letter provided
+second = str(input("Enter Second Letter: ")).lower(
+)  #Asks the user for a letter to search in text
+print("First Letter:", second)  #Prints the letter provided
+third = str(input("Enter Third Letter: ")).lower(
+)  #Asks the user for a letter to search in text
+print("Third Letter:", third)  #Prints the letter provided
 
 #the reason for the .lower is so when our code is searching for the letter provided by the user it will find the total amount of letters in the given text because we lowered the text
-
 
 searchfirst = text.count(first)
 print(f"The first letter appears {searchfirst} times")
@@ -20,23 +22,24 @@ searchthird = text.count(third)
 print(f"The third letter appears {searchthird} times")
 
 letters = {
-  "firstletter": searchfirst,
-  "secondletter": searchsecond,
-  "thirdletter": searchthird
-  }
+    "firstletter": searchfirst,
+    "secondletter": searchsecond,
+    "thirdletter": searchthird
+}
 wordsInText = len(text.split())
 print("The amount of words in the text is {} words".format(wordsInText))
 
 first_letter = text[0]
 last_letter = text[-1]
-print("The first letter of the text is {} and the last letter is {}".format(first_letter, last_letter))
+print("The first letter of the text is {} and the last letter is {}".format(
+    first_letter, last_letter))
 
 # reversing words in a given string
-a = text.split()[::-1] 
+a = text.split()[::-1]
 b = []
-for i in a: #i is a temporary variable that stores a value
-    # appending reversed words to l
-    b.append(i)
+for i in a:  #i is a temporary variable that stores a value
+  # appending reversed words to l
+  b.append(i)
 # printing reverse words
 print("Reversed Order: ")
 print(" ".join(b))
@@ -44,8 +47,3 @@ print(" ".join(b))
 specialWord = "Python" and "python"
 result = specialWord in text
 print("Python in text = {}".format(result))
-
-
-
-
-
